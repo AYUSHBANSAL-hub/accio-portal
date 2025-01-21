3be18175a7aaaf8d6fc265a3216885e8239d6575
ayush bansal <forstudyonly101@gmail.com>
2025-01-21 17:18:17 +0530

    added config

--- src/app/components/reports.tsx
+++ src/app/components/reports.tsx
@@ -77,21 +77,21 @@ const Reports = () => {
         <img
           src="https://acciojob.com/src/Navbar/logo.svg"
           alt="Logo"
-          className="w-20 h-20 m-auto rounded-full"
+          className="w-auto h-20 m-auto rounded-full"
         />
-        <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#307fec] to-[#022555] text-center mb-8 mt-5 animate-title">
+        <h1 className="text-4xl font-bold text-black text-center mb-8 mt-5">
           AVAILABLE REPORTS
         </h1>
 
         {availableReports && availableReports.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {availableReports.map((report, index) => (
+            {availableReports.map((report:any, index:any) => (
               <div
                 key={index}
                 className="bg-[#1c2533] p-6 rounded-xl shadow-2xl hover:shadow-2xl hover:scale-105 transform transition-all duration-300 ease-in-out"
               >
                 <div className="flex justify-between items-center mb-5">
-                  <div className="text-2xl font-semibold text-[#307fec] hover:text-white transition-colors duration-300">
+                  <div className="text-2xl font-semibold text-[#fffff] hover:text-white transition-colors duration-300">
                     {report.reportname}
                   </div>
                   <img
@@ -103,7 +103,7 @@ const Reports = () => {
                 <p className="text-[#e5e7eb] text-sm mb-4">
                   {report.description}
                 </p>
-                <button className="px-4 py-2 rounded-full text-white bg-[#307fec] hover:bg-[#1e77d0] transition-colors duration-300">
+                <button className="px-4 py-2 rounded text-white bg-[#307fec] hover:bg-[#1e77d0] transition-colors duration-300">
                   <Link href={`/components/${report.path}`}>View Report</Link>
                 </button>
               </div>
